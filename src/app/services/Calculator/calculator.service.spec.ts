771edfa5e25a0a48d9c583b186f4ad4f5ac4a009
Viplav Patil <viplav.patil@nagarro.com>
2022-11-21 09:29:53 +0530

    video-6 completed

--- src/app/services/Calculator/calculator.service.spec.ts
+++ src/app/services/Calculator/calculator.service.spec.ts
@@ -3,6 +3,7 @@ import { CalculatorService } from './calculator.service';
 describe('CalculatorService', () => {
   it('should add two numbers', () => {
     const calculator = new CalculatorService();
+    //fail
     let result = calculator.add(2, 2);
     expect(result).toBe(4);
   });
@@ -11,5 +12,18 @@ describe('CalculatorService', () => {
     let calculator = new CalculatorService();
     let result = calculator.subtract(2, 2);
     expect(result).toBe(0);
+  });
+
+  it('should be in pending state', () => {
+    pending()     //if inserted without any code it displays as pending
+  });
+
+  
+  it('should always fail', () => {
+    fail()     //if inserted without any code it always fail
+  });
+
+  xit('should always disabled', () => {
+    fail()     //if inserted without any code it always fails
   });
 });
