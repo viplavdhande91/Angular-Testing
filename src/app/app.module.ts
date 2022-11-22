defe51c5d69d7d14cafdb423352760a5e5620bbc
Viplav Patil <viplav.patil@nagarro.com>
2022-11-22 12:32:53 +0530

    11 video done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StrengthPipe } from './pipes/Strength/strength.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StrengthPipe
   ],
   imports: [
     BrowserModule,
