9f1b07ae9ff57316add5943239220cab748f6793
Viplav Patil <viplav.patil@nagarro.com>
2022-11-22 17:56:56 +0530

    12-video completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StrengthPipe } from './pipes/Strength/strength.pipe';
 import { PostsComponent } from './components/posts/posts.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
-  declarations: [AppComponent, StrengthPipe, PostsComponent],
+  declarations: [AppComponent, PostsComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/models/Post.ts
+++ src/app/models/Post.ts
@@ -1,4 +1,5 @@
 export interface Post {
+  userId:number,
   id: number;
   title: string;
   body: string;
